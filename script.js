830de77374198677cbbc190d5df880ebf44c5c54
AlonRTPve <Alonshlomi@hotmail.com>
2022-11-26 18:03:50 +0200

    added rainbow styling, added the option to let the user to choose num of boxes

--- script.js
+++ script.js
@@ -5,12 +5,17 @@ btn_wipe_board.addEventListener("click", wipe_board);
 color.addEventListener("click", Set_Random_Color);
 const something = document.getElementsByClassName("active");
 
+let rainbow_color = false;
+let number = 0;
+arrayofcolors = ["orange", "green", "blue", "yellow", "pink"];
+
+var num_of_boxes = prompt("Enter number of boxes: ");
 
 
-
-for (let i = 0; i < 16; i++) {      //creates the board 
+for (let i = 0; i < num_of_boxes; i++) {      //creates the board 
   const div = document.createElement('div');
   div.setAttribute("id", "changeit")
+  div.setAttribute("class", "default")
   div.textContent = (" ")
   cdiv.appendChild(div);
 }
@@ -18,10 +23,23 @@ for (let i = 0; i < 16; i++) {      //creates the board
 const div = document.querySelectorAll('#changeit');
 
 
-div.forEach(divon => {  //sets the background color of the grids/divs to red
-    divon.addEventListener("mouseover", (event) => {
-      divon.setAttribute("class", "active")
+div.forEach(divon => {  //sets the background color of the grids/divs to red / rainbow 
+    divon.addEventListener("mouseover", (event) => { 
+    if (divon.className === "notactive" || divon.className === "default"){
+       
+         if (rainbow_color == false){
+                divon.setAttribute("class", "active")
+            }
+         else{
+             var random = arrayofcolors[Math.floor(Math.random()*arrayofcolors.length)]
+             divon.setAttribute("class", random)
+         }
+    }
+    else{
+        console.log()
+    }
     })
+
   })
 
 
@@ -31,12 +49,11 @@ function wipe_board(){          //functions that sets the background color of th
     })
 }
 
-arrayofcolors = ["red", "green", "blue", "yellow", "pink"];
 
+ 
 
 function Set_Random_Color(div) { //wipes the board and sets random color
     wipe_board()
-    var random = arrayofcolors[Math.floor(Math.random()*arrayofcolors.length)]
-
+    rainbow_color = true;
 }
 
